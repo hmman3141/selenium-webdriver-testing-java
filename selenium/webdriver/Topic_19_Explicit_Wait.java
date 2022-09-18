d2791887a3a10deaa99f45b891610278ceb4d2f0
Hua Minh Man <51411693+em1234tiger@users.noreply.github.com>
2022-09-18 17:47:10 +0700

    main, Topic_19 update TC_01_02_03 add TC_04

--- selenium/webdriver/Topic_19_Explicit_Wait.java
+++ selenium/webdriver/Topic_19_Explicit_Wait.java
@@ -1,5 +1,9 @@
 package webdriver;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -29,7 +33,7 @@ public class Topic_19_Explicit_Wait {
 	}
 
 	@Test
-	public void TC_01_Enough_Time() {
+	public void TC_01_Invisible() {
 		// Open website
 		driver.get("https://automationfc.github.io/dynamic-loading/");
 
@@ -43,14 +47,14 @@ public class Topic_19_Explicit_Wait {
 		driver.findElement(By.cssSelector("div#start>button")).click();
 
 		// Wait for text
-		explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#finish>h4")));
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div#loading")));
 
 		// Verify text
 		Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), text);
 	}
 
 	@Test
-	public void TC_02_Plenty_Of_Time() {
+	public void TC_02_Visible() {
 		// Open website
 		driver.get("https://automationfc.github.io/dynamic-loading/");
 
@@ -64,31 +68,76 @@ public class Topic_19_Explicit_Wait {
 		driver.findElement(By.cssSelector("div#start>button")).click();
 
 		// Wait for text
-		explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#finish>h4")));
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#finish>h4")));
 
 		// Verify text
 		Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), text);
 	}
 
 	@Test
-	public void TC_03_Not_Enough_Time() {
+	public void TC_03_Telerik() {
 		// Open website
-		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.get(
+				"https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
 
 		// Declaration
-		String text = "Hello World!";
+		String day = "19";
 
 		// Set explicitly wait
-		explicitWait = new WebDriverWait(driver, 2);
+		explicitWait = new WebDriverWait(driver, 10);
 
-		// Click "Start" button
-		driver.findElement(By.cssSelector("div#start>button")).click();
+		// Wait for table
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.calendarContainer")));
+		explicitWait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//table[contains(@id,'RadCalendar')]//a[text()='19']")));
 
-		// Wait for text
-		explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#finish>h4")));
+		// Click "19" button
+		driver.findElement(By.xpath("//table[contains(@id,'RadCalendar')]//a[text()='19']")).click();
+
+		// Wait for waiting icon disappear
+		explicitWait.until(
+				ExpectedConditions.stalenessOf(driver.findElement(By.cssSelector("div[id*='RadCalendar'] div.raDiv"))));
 
 		// Verify text
-		Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), text);
+		Assert.assertTrue(driver.findElement(By.cssSelector("div.datesContainer span[id*='ContentPlaceholder']"))
+				.getText().contains(day));
+	}
+
+	@Test
+	public void TC_04_GoFile() {
+		// Open website
+		driver.get("https://gofile.io/uploadFiles");
+
+		// Declaration
+		String imgFolderPath = projectPath + File.separator + "img" + File.separator;
+		List<String> files = Arrays.asList(imgFolderPath + "img1.jpg", imgFolderPath + "img2.jpg",
+				imgFolderPath + "img3.jpg");
+		String filePath = "";
+
+		// Set explicitly wait
+		explicitWait = new WebDriverWait(driver, 10);
+
+		// Wait for button to be visible
+		explicitWait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector("div#rowUploadButton button[class*='uploadButton']")));
+
+		// Send files
+		filePath = String.join("\n", files);
+		driver.findElement(By.cssSelector("input[type='file']")).sendKeys(filePath);
+
+		// Wait for "Show files" appear
+		explicitWait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#rowUploadSuccess-showFiles")));
+
+		// Click "Show files" button
+		driver.findElement(By.cssSelector("button#rowUploadSuccess-showFiles")).click();
+
+		// Wait for all files to appear
+		explicitWait.until(ExpectedConditions
+				.visibilityOfAllElements(driver.findElements(By.cssSelector("div#rowFolder-tableContent>div"))));
+
+		// Verify number of files
+		Assert.assertEquals(driver.findElements(By.cssSelector("div#rowFolder-tableContent>div")).size(), 3);
 	}
 
 	@AfterClass
